0c76e1ee1a99de2529d6bbdaccff5dd751a9eeab
mariiarode <rodriguezdenizmaria@gmail.com>
2024-12-21 13:39:15 +0000

    Main part of the portfolio
    I just programmed the first and main part of the portfolio. I am using react.

--- My Portfolio/src/App.jsx
+++ My Portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from "./App.module.css";
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Navbar/Hero/Hero';
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ My Portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { getImageUrl } from '../../../utils'
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm María</h1>
+            <p className={styles.description}>I am a computer engineering student.  Motivated and passionate about continuous learning and self improvement, with a strong drive to acquire new skills and
+ contribute to team success. 
+ Hardworking, ambitious, creative, and enthusiastic.</p>
+
+            <a href="mailto:rodriguezdenizmaria@gmail.com" className={styles.contact}>Contact Me!</a>
+        </div>
+        <img src={getImageUrl("IMG_8473_jpg.JPG")} alt="Image of me" className={styles.image}/>
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+    </section>
+      
+  )
+}
+
+export default Hero
